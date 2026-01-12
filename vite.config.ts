3275f55d62eb26a196da1bd768a37f1d068fc0ec
Guilherme Moura <glhrmoura@gmail.com>
2026-01-12 09:48:28 -0300

    chore: add vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default {
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}
